c76eaac4fcd027dba8f515d9ae3b3e17e7e72761
stephanetchatchum@gmail.com <stephanetchatchum@gmail.com>
2026-06-17 12:08:11 +0200

    testing if the form saves

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,11 @@ const form = document.querySelector("#transaction-form");
 
 form.addEventListener("submit", function(event){
     event.preventDefault();
-    console.log("Form sumbmitted!");
+    
+    const description = document.querySelector('#description').value;
+    const amount = document.querySelector('#amount').value;
+    const category = document.querySelector('#category').value;
+    const date = document.querySelector('#date').value;
+
+    console.log(description, amount, category, date);
 });
